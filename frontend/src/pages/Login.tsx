e7931c995bbd209c9651f0a029d5888b8f7439a5
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-06 17:13:32 -0300

    feat: insere função de login.

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,30 @@
 import { useState } from "react";
+import { MouseEvent } from "react";
+import { Navigate } from "react-router-dom";
+import { requestLogin, setToken } from "../services/requests";
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isLogged, setIsLogged] = useState(false);
+
+  const handleLogin = async (event: MouseEvent) => {
+    event.preventDefault();
+
+    try {
+      const { token } = await requestLogin("/login", { username, password });
+
+      setToken(token);
+
+      localStorage.setItem("token", token);
+
+      setIsLogged(true);
+    } catch (error) {
+      setIsLogged(false);
+    }
+  };
+
+  if (isLogged) return <Navigate to="/clients" />;
 
   return (
     <>
@@ -24,7 +46,9 @@ export default function Login() {
               placeholder="Senha"
             />
           </label>
-          <button type="submit">Entrar</button>
+          <button type="submit" onClick={(event) => handleLogin(event)}>
+            Entrar
+          </button>
         </form>
       </section>
     </>
