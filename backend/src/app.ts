f49e9b28766c162bae6e266e4e85d7d4cf208506
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-06 13:53:37 -0300

    feat: insere middleware de autenticação.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import authMiddleware from "./middlewares/auth.middleware";
 import errorMiddleware from "./middlewares/error.middleware";
 import userRoutes from "./routes/login.routes";
 
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/login", userRoutes);
+app.use(authMiddleware);
 app.use("/user", userRoutes);
 
 app.use(errorMiddleware);
