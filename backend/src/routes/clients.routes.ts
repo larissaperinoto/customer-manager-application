b7c48d9e190af29cffbb0f1f801e2b2856f3b2d1
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-06 15:58:31 -0300

    feat: adiciona middleware de validações na rota clients.

--- backend/src/routes/clients.routes.ts
+++ backend/src/routes/clients.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import clientsController from "../controllers/clients.controller";
+import validateClient from "../middlewares/clients.middleware";
 
 const routes = Router();
 
 routes.delete("/:id", clientsController.delete);
 routes.put("/:id", clientsController.updateById);
+routes.post("/", validateClient, clientsController.create);
 routes.get("/", clientsController.findAll);
-routes.post("/", clientsController.create);
 
 export default routes;
