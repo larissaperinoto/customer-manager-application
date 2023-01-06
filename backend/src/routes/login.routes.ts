bae839b054c17c451e8296acda468a01d0b22491
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-06 15:58:59 -0300

    feat: insere middleware de validações na rota login.

--- backend/src/routes/login.routes.ts
+++ backend/src/routes/login.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import userController from "../controllers/user.controller";
+import validateLogin from "../middlewares/login.middleware";
 
 const routes = Router();
 
-routes.post("/", userController.login);
+routes.post("/", validateLogin, userController.login);
 
 export default routes;
