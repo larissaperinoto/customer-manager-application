4ebfe0f710d8163893f738097385ceabd69adcad
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-06 13:19:33 -0300

    feat: adiciona conexão com o banco MongoDB.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,12 @@
 import app from "./app";
+import mongoose from "mongoose";
 import { config } from "dotenv";
 
 config();
+
+mongoose.connect(
+  process.env.MONGO_URI || "mongodb://localhost:27017/sharenergy"
+);
 
 const PORT = process.env.SERVER_PORT || 3001;
 
