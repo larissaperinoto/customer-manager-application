f10b26ff363e3d42bf1d253aaec33b4f9014d550
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-08 18:43:55 -0300

    fix: corrige redirecionamento de página no header.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,21 +1,12 @@
-import { Navigate } from "react-router-dom";
 import "../style/Header.css";
 
 export default function Header() {
   return (
     <header>
-      <button type="button" onClick={() => <Navigate to="/users" />}>
-        Users
-      </button>
-      <button type="button" onClick={() => <Navigate to="/dogs" />}>
-        Dogs
-      </button>
-      <button type="button" onClick={() => <Navigate to="/cats" />}>
-        HttpCats
-      </button>
-      <button type="button" onClick={() => <Navigate to="/clients" />}>
-        Clientes
-      </button>
+      <a href="/users">Users</a>
+      <a href="/dogs">Dogs</a>
+      <a href="/cats">HttpCats</a>
+      <a href="/clients">Clientes</a>
     </header>
   );
 }
