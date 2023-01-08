46418d9516089505a2d20c007ccd2e40210730b3
Larissa Perinoto <perinotolarissa@gmail.com>
2023-01-08 18:49:07 -0300

    feat: insere botão sair ao header.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,19 @@
 import "../style/Header.css";
 
 export default function Header() {
+  function leave() {
+    localStorage.setItem("token", "");
+  }
+
   return (
     <header>
       <a href="/users">Users</a>
       <a href="/dogs">Dogs</a>
       <a href="/cats">HttpCats</a>
       <a href="/clients">Clientes</a>
+      <a href="/Login" onClick={() => leave()}>
+        Sair
+      </a>
     </header>
   );
 }
